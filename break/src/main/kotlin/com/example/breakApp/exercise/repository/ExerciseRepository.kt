3764b901dd587f7ad95e5787c6dd3cc86e9f9911
GyuCastle <djdjdhdjdhdje@gmail.com>
2024-11-01 18:37:16 +0900

    Update ExerciseRepository.kt

--- break/src/main/kotlin/com/example/breakApp/exercise/repository/ExerciseRepository.kt
+++ break/src/main/kotlin/com/example/breakApp/exercise/repository/ExerciseRepository.kt
@@ -2,7 +2,10 @@ package com.example.breakApp.exercise.repository
 
 import com.example.breakApp.exercise.entity.Exercise
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ExerciseRepository : JpaRepository<Exercise, Long> {
+    // 운동 이름으로 조회하는 메서드
     fun findByName(name: String): Exercise?
 }
