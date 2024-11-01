b77fdf0af5af5c636b59a4f84f6f4ef262bccffc
GyuCastle <djdjdhdjdhdje@gmail.com>
2024-11-01 19:17:28 +0900

    Update Exercise Entity.kt

--- break/src/main/kotlin/com/example/breakApp/exercise/entity/Exercise Entity.kt
+++ break/src/main/kotlin/com/example/breakApp/exercise/entity/Exercise Entity.kt
@@ -1,31 +1,23 @@
 package com.example.breakApp.exercise.entity
 
 import jakarta.persistence.*
-import java.sql.Timestamp
 
 @Entity
-@Table(name = "exercise")
+@Table(name = "exercise", uniqueConstraints = [UniqueConstraint(columnNames = ["name"])])
 data class Exercise(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "exercise_id")
-    val exerciseId: Long = 0,
+    val exerciseId: Long? = null,
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     val name: String,
 
     @Column(columnDefinition = "TEXT")
-    val description: String? = null,
+    val description: String,
 
     @Column(nullable = false)
     val category: String,
 
-    @Column(name = "target_area", nullable = false)
-    val targetArea: String,
-
-    @Column(name = "created_at", updatable = false)
-    val createdAt: Timestamp = Timestamp(System.currentTimeMillis()),
-
-    @Column(name = "updated_at")
-    val updatedAt: Timestamp = Timestamp(System.currentTimeMillis())
+    @Column(nullable = false)
+    val targetArea: String
 )
